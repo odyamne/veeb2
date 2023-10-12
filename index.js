6df9e2f6a5e2cfdc061ef2d5adfbfb8ae74a8cf1
Ander Aava <43364845+odyamne@users.noreply.github.com>
2023-10-12 17:02:42 +0300

    ExpressJS 1.tund final

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
+const dateInfo = require('./datetime_ET');
 
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//routes
 app.get('/', (req, res) =>{
-    res.send('OKKKKKKKKK');
+    //res.send('OKKKKKKKKK');
+    res.render('index');
 });
 
-app.get('/test', (req, res) =>{
-    res.send('ok test.');
+app.get('/timenow', (req, res) =>{
+    const dateNow = dateInfo.dateNowET();
+    const timeNow = dateInfo.timeNowET();
+    res.render('timenow', {dateN: dateNow, timeN: timeNow});
+});
+
+app.get('/proverb', (req, res) =>{
+    let proverb = [];
+    fs.readFile("public/txtfiles/vanasonad.txt", "utf8", (err, data)=>{	
+        if(err){
+            console.log(err);
+        }
+        else{
+            proverb = data.split(";");
+            res.render('justlist', {h1: 'Vanasõnad', proverbs: proverb});   
+        }
+    });
 });
 
 app.listen(5210);
